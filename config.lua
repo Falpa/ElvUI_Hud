8f8ff262768545bc0baa0730ba757d21c0005051
Michael Tindal <babyplatypus@me.com>
2010-11-14 20:45:46 -0600

    Use Tukui's healthcolor.

--- config.lua
+++ config.lua
@@ -3,7 +3,6 @@ TukuiHudCF = {
   pethud = true, -- show the pet hud
   showthreat = true, -- show a threat bar next to the players hud
   unicolor = true, -- use a unicolor them
-  healthcolor = {.05, .05, .05}, -- health color of hud bars when unicolor is enabled
   showsmooth = true, -- show smooth bars
   hideooc = true, -- hide the hud out of combat
   height = 150, -- height of the HUD
