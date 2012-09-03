b0cd5958fb9afa761404801e466bb5168184b902
Michael Tindal <babyplatypus@me.com>
2012-09-03 17:56:41 -0500

    Update locales.  Thanks Edoc.

--- locales/spanish.lua
+++ locales/spanish.lua
@@ -3,76 +3,73 @@ local L = AceLocale:NewLocale("ElvUI", "esES") or AceLocale:NewLocale("ElvUI", "
 if not L then return end
 
 L["ElvUI Hud "] = true
-L[" is loaded. Thank you for using it and note that I will always support you."] = true
+L[" is loaded. Thank you for using it and note that I will always support you."] = " ha sido cargado. Gracias por utilizarlo y tenga en cuenta que siempre le ayudaré"
 L["Hud"] = true
-L["Enable the Hud."] = true
-L["Reset Settings"] = true
-L['Simple Layout'] = true
-L['Use the simple layout from 2.0'] = true
-L["Hud Options"] = true
-L["Hide ElvUI Unitframes"] = true
-L["Flash"] = true
-L["Flash health/power when the low threshold is reached"] = true
-L["Text Warning"] = true
-L["Show a Text Warning when the low threshold is reached"] = true
-L["Horizontal Castbar"] = true
-L["Use a horizontal castbar"] = true
+L["Enable the Hud."] = "Activar el Hud."
+L["Reset Settings"] = "Reiniciar Configuración"
+L['Simple Layout'] = "Diseño Sencillo"
+L['Use the simple layout from 2.0'] = "Usar el diseño sencillo de la versión 2.0"
+L["Hud Options"] = "Opciones del HUD"
+L["Hide ElvUI Unitframes"] = "Ocultar los marcos de unidad de ElvUI"
+L["Flash"] = "Destello"
+L["Flash health/power when the low threshold is reached"] = "Destellar vida/poder cuanto se alcance el umbral"
+L["Text Warning"] = "Aviso Textual"
+L["Show a Text Warning when the low threshold is reached"] = "Mostrar un Aviso Textual cuando se alcance el umbral"
+L["Horizontal Castbar"] = "Barra de hechizos horizontal"
+L["Use a horizontal castbar"] = "Usar una barra de hechizos horizontal"
 L["Variables"] = true
-L["Set the Alpha of the Hud when out of combat"] = true
-L["Out of Combat Alpha"] = true
-L["Start flashing health/power under this percentage"] = true
-L["Raid Icon"] = true
-L["Combat Indicator"] = true
-L["PVP Text"] = true
-L["Wild Mushroom Tracker"] = true
-L["GCD Spark"] = true
-L["Player Hud"] = true
-L["Target Hud"] = true
-L["Pet Hud"] = true
-L["Target Target Hud"] = true
-L["Pet Target Hud"] = true
-L["Point From"] = true
-L["Position on the element to anchor"] = true
-L['What to attach this element to.'] = true
-L['Point To'] = true
+L["Set the Alpha of the Hud when out of combat"] = "Ajustar la transparencia del Hud fuera de combate"
+L["Out of Combat Alpha"] = "Transparencia fuera de combate"
+L["Start flashing health/power under this percentage"] = "Destellar vida/poder por debajo de este porcentaje"
+L["Raid Icon"] = "Icono de Banda"
+L["Combat Indicator"] = "Indicador de Combate"
+L["PVP Text"] = "Texto JcJ"
+L["Wild Mushroom Tracker"] = "Marcador de Champiñones salvajes"
+L["GCD Spark"] = "Destello de GCD"
+L["Player Hud"] = "Hud de Jugador"
+L["Target Hud"] = "Hud de Objetivo"
+L["Pet Hud"] = "Hud de Mascota"
+L["Target Target Hud"] = "Hud de Objetivo de Objetivo"
+L["Pet Target Hud"] = "Hud de Objetivo de Mascota"
+L['What to attach this element to.'] = "A qué fijar este elemento"
 L["Eclipse"] = true
-L["Override"] = true
-L["Override the texture for this element"] = true
-L['ElvUI Hud by Sortokk'] = true
-L['ElvUI Hud provides a configurable Heads Up Display for use with ElvUI.\n'] = true
-L['Reset the settings of this addon to their defaults.'] = true
-L['Hide the ElvUI Unitframes when the Hud is enabled'] = true
-L['Hide the Hud when out of Combat'] = true
-L['Hide Out of Combat'] = true
-L['Enable Mouse'] = true
-L['Enable the mouse to interface with the hud (this option has no effect if ElvUI Unitframes are hidden)'] = true
-L['Set the Alpha of the Hud when in combat'] = true
-L['Position on the attached element to anchor to'] = true
-L['The texture that will be used for statusbars on this element.'] = true
-L['Override the font for this element'] = true
-L['Set the font for this element'] = true
-L['Set the font size for this element'] = true
-L['Spacing'] = true
-L["Texture"] = true
-L['Tick Color'] = true
-L['Value'] = true
-L['Default'] = true
-L["ELVUI_HUD_CREDITS"] = [[Many thanks to the following people:
+L["Override"] = "Sobrescribir"
+L["Override the texture for this element"] = "Sobrescribir la textura para este elemento"
+L['ElvUI Hud by Sortokk'] = "ElvUI Hud por Sortokk"
+L['ElvUI Hud provides a configurable Heads Up Display for use with ElvUI.\n'] = "ElvUI Hud proporciona un nuevo conjunto de marcos de unidad"
+L['Reset the settings of this addon to their defaults.'] = "Reiniciar la configuración de este addon a sus valores predeterminados"
+L['Hide the ElvUI Unitframes when the Hud is enabled'] = "Ocultar los marcos de unidad de ElvUI cuando el Hud esté activado"
+L['Hide the Hud when out of Combat'] = "Ocultar el Hud fuera de combate"
+L['Hide Out of Combat'] = "Ocultar fuera de combate"
+L['Enable Mouse'] = "Activar Ratón"
+L['Enable the mouse to interface with the hud (this option has no effect if ElvUI Unitframes are hidden)'] = "Permitir al ratón comunicarse con el Hud (Esta opción no tendrá efecto si los marcos de unidad de ElvUI están ocultos"
+L['Set the Alpha of the Hud when in combat'] = "Ajustar la transparencia del hud en combate"
+L['The texture that will be used for statusbars on this element.'] = "La textura que será usada para las barras de este elemento"
+L['Override the font for this element'] = "Sobrescribir la fuente para este elemento"
+L['Set the font for this element'] = "Ajustar la fuente para este elemento"
+L['Set the font size for this element'] = "Ajustar el tamaño de fuente para este elemento"
+L['Spacing'] = "Separación"
+L["Texture"] = "Textura"
+L['Tick Color'] = "Color de los Tic"
+L['Value'] = "Valor"
+L['Default'] = "Predeterminado"
+L["ELVUI_HUD_CREDITS"] = [[Quiero dar las gracias a las siguientes personas:
 
-Tukz whose unitframe code was the basis for the Hud
-Elv for his amazing UI which was the inspiration for version 3
-Hydrazine for inspiration
-Boradan for help with the options
-Sgt.Hydra for suggestions
-Darth Predator for the Russian translation
-NOme for the Korean translation
-BuG for the French translation
+Tukz, cuyos marcos de unidad han sido los cimientos para el Hud
+Elv, por su impresionante UI que ha sido la inspiración para la versión 3
+Hydrazine, por la inspiración
+Boradan, por ayudarme con las opciones
+Sgt.Hydra, por todas las sugerencias
+Darth Predator, por la traducción a Ruso
+NOme, por la traducción a Koreano
+BuG, por la traducción a Francés
+Edoc, por la traducción a Español
 
-The Tukui community for being supportive
+Y a la comunidad de Tukui por todo el apoyo.
 ]]
 
-L["Thank you for using ElvUI Hud!"] = true
-L["Here you can choose between the simple layout (only player health and power) or the default layout for the hud"] = true
-L["Simple Layout"] = true
-L["Default Layout"] = true
-L["Simple Layout Set"] = true
+L["Thank you for using ElvUI Hud!"] = "Gracias por usar ElvUI Hud"
+L["Here you can choose between the simple layout (only player health and power) or the default layout for the hud"] = "Aquí puedes elegir entre el diseño sencillo (solo vida y poder) o el diseño predeterminado del Hud"
+L["Simple Layout"] = "Diseño Sencillo"
+L["Default Layout"] = "Diseño Predeterminado"
+L["Simple Layout Set"] = "Ajustado al Diseño Sencillo"
